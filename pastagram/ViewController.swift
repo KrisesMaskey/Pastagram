3170b6081b72230c9841f40531677cf3b0499460
Krises Maskey <84721964+KrisesMaskey@users.noreply.github.com>
2021-10-19 13:46:51 +0400

    Initial Commit

---
+++ pastagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pastagram
+//
+//  Created by NYUAD on 10/19/21.
+//  Copyright © 2021 NYUAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
